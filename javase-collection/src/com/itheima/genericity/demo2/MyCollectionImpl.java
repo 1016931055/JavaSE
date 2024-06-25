b2c6f958f1915af33b2ca581ded6586d2e84f9f6
Shen Zhaolong <144757916+1016931055@users.noreply.github.com>
2024-06-25 12:19:35 +0800

    Delete javase-collection directory

--- javase-collection/src/com/itheima/genericity/demo2/MyCollectionImpl.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.genericity.demo2;
-
-//子类在实现接口时，明确了接口上泛型的具体类型
-public class MyCollectionImpl implements MyCollection<Integer>{
-    @Override
-    public void add(Integer param) {
-
-    }
-}
